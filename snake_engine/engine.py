512d9381eda0a34e167270f2ea9eb9e4488e332e
John B <JohnBee@users.noreply.github.com>
2020-12-23 18:11:56 +0000

    Added printing and food generation

--- snake_engine/engine.py
+++ snake_engine/engine.py
@@ -1,24 +1,85 @@
+from random import randint
+
 class Engine:
     def __init__(self, world_width = 10, world_height = 10):
-        ## Generate empty world
-        self.world = self.gen_world(world_width, world_height)
-        ## Initialise the snake
-        self.snake = Snake(int(world_width)/2, int(world_height)/2, 3)
+        '''
+        Initialise the snake engine where all game operations will take place.
 
-    @staticmethod
-    def gen_world(width, height):
-        world_out = ["."*width]*height
-        return world_out
+        :param world_width: Width of the game world the snake should roam
+        :param world_height: Height of the game world the snake should roam
+        '''
+
+        self.world_width = world_width
+        self.world_height = world_height
+        self.food = []
+
+        ## Initialise the snake
+        self.snake = Snake(world_width//2, world_height//2, 4)
+        self.add_food()
+
+    def add_food(self):
+        '''
+        Add food to the game world, possible locations are only where the snake isn't
+        :return: True or False depending if food was able to be added, if it false then the game must be complete.
+        '''
+        possible_locations = [(x, y) for x in range(self.world_width) for y in range(self.world_height)]
+        for s_not_possible in self.snake.body + self.food:
+            if s_not_possible in possible_locations:
+                possible_locations.remove(s_not_possible)
+
+        if not possible_locations:
+            return False
+        else:
+            # select a possible location
+            self.food.append(possible_locations[randint(0, len(possible_locations)-1)])
+            return True
+
+    def output_world(self):
+        '''
+        Output the game world as a list of list of characters to the parsed by AI or printed
+        :return:
+        '''
+        out = []
+        for y in range(self.world_height):
+            out.append([])
+            for x in range(self.world_width):
+                if (x, y) not in self.food and (x, y) not in self.snake.body:
+                    out[-1].append(".")
+                elif (x, y) in self.food:
+                    out[-1].append("o")
+                elif (x, y) in self.snake.body:
+                    if(x, y) == self.snake.body[-1]:
+                        if self.snake.direction == 0:
+                            out[-1].append(">")
+                        if self.snake.direction == 1:
+                            out[-1].append("^")
+                        if self.snake.direction == 2:
+                            out[-1].append("<")
+                        if self.snake.direction == 3:
+                            out[-1].append("v")
+                    else:
+                        out[-1].append("#")
+        return out
+
+    def pretty_print_world(self):
+
+        for y in self.output_world():
+            print(" ".join(y))
+
 
 
 class Snake:
     def __init__(self, pos_x=0, pos_y=0, length=3):
-        self.head_x = pos_x
-        self.head_y = pos_y
-
         # init tail of given length
-        self.tail = self.gen_tail(self.head_x, self.head_y, length)
+        self.head = [(pos_x, pos_y)]
+        self.direction = 0 # 0 = right, 1 = up, 2 = left, 3 = down
+        self.body = self.gen_tail(pos_x, pos_y, length) + self.head
 
     @staticmethod
     def gen_tail(head_x, head_y, length=3):
         return [(x, head_y) for x in range(head_x, head_x-length, -1)]
+
+
+if __name__ == "__main__":
+    e = Engine()
+    e.pretty_print_world()
--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,15 +1,18 @@
 from unittest import TestCase
-from snake_engine.engine import Engine, Snake
-
-
-class TestEngine(TestCase):
-    def test_gen_world(self):
-        t_world = Engine.gen_world(10, 10)
-        self.assertEqual(len(t_world), 10, "Array wrong height")
-        self.assertEqual(len(t_world[0]), 10, "Array wrong width")
+from SnakeAi.snake_engine.engine import Engine, Snake
 
 
 class TestSnake(TestCase):
     def test_gen_tail(self):
-        t_tail = Snake.gen_tail(0,0,length=10)
+        t_tail = Snake.gen_tail(0, 0, length=10)
         self.assertEqual(len(t_tail), 10, "snake tail correct length")
+
+
+class TestEngine(TestCase):
+    def test_add_food(self):
+        e = Engine()
+        # fill up game with food until false
+        while e.add_food():
+            pass
+        self.assertEqual(96, len(e.food)) # exactly 96 food for all space, 100 spaces - 4 for snake = 96
+
