856c3083c9b6741834757b21338e284308ec8b85
John B <JohnBee@users.noreply.github.com>
2020-12-29 15:10:30 +0000

    updated commands
    + demo gif
    + updated readme
    updated commands in argparser

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
 # SnakeAI
 
 AI to simlulate and play the game snake in attempt achieve maximum score.
+
+## Method
+The Snake uses the "Q-learning" Reinforcement learning technique to best pick it's next move.
+The Snake does not play perfectly, this is due to the limits of space storage in basic Q-learning. 
+The ammount of storage required is given by `States x Actions`.
+
+The snake has a limited number of sensors:
+
+```
+Food:
+Above, Below, or Inline
+Left, Right, or Inline
+
+Danger:
+Ahead, Left, or Right
+```
+A limit number of moves:
+```
+Turn Left, Turn Right, Go Forward
+```
+This provides a total of 288 possible states. The addition of extra states causes exponential growth.
+
+## Running SnakeAI
+Running the existing provided Q-Table.
+```
+$ python SnakeAI.py
+```
+Overriding the existing Q-Table with new values and no gameplay display.
+```
+$ python SnakeAI.py --learn 100000 --no-display
+```
+## Demo
+![snake_ai](demo/snake_1.gif)
--- snakeAI.py
+++ snakeAI.py
@@ -5,15 +5,16 @@ from AI.q_learning import run_q_learn
 
 def run(args):
     should_learn = True if args.learn is not None else False
-    if not args.display:
-        run_display(args.learn, args.learn)
+    if args.nodisplay:
+        run_q_learn(args.learn, should_learn)
     else:
-        run_q_learn(args.learn, args.learn)
+        run_display(args.learn, should_learn)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Python Q-Learning Agent that learns to play the game Snake")
     parser.add_argument('--learn', dest="learn", type=int, help="The number of episodes (games) for the Snake to learn from (default 100000). If not specified will use prelearnt Q-table instead.", default=None)
-    parser.add_argument('--no-display', dest="display", action="store_true", help="Do not use Pygame to display the final Snake gameplay.", default=False)
+    parser.add_argument('--no-display', dest="nodisplay", action="store_true", help="Do not use Pygame to display the final Snake gameplay.", default=False)
     args = parser.parse_args()
+    print(args)
     run(args)
